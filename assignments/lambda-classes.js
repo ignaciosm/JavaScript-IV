9e8fe67cbcf79c2b910594298021b591bc564e8a
ignaciosm <ignaciosm@gmail.com>
2019-09-19 14:54:35 -0500

    build class instructor and student and test ok

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -11,14 +11,65 @@ class Person {
   }
 }
 
+class Instructor extends Person {
+  constructor(props){
+    super(props),
+    this.specialty = props.specialty,
+    this.favLanguage = props.favLanguage,
+    this.catchPhrase = props.catchPhrase
+  }
+  demo(){
+    return `Today we are learning about ${this.specialty}`;
+  }
+  grade(){
+    return `${this.name} receives a perfect score on ${this.specialty}`
+  }
+}
+
+class Student extends Person {
+  constructor(props){
+    super(props),
+    this.previousBackground = props.previousBackground,
+    this.className = props.className,
+    this.favSubjects = props.favSubjects
+  }
+  listsSubjects(){
+    return this.favSubjects;
+  }
+  PRAssignment(){
+    return `${this.name} has submitted a PR for ${this.favSubjects[0]}`;
+  }
+  sprintChallenge(){
+    return `${this.name} has begun sprint challenge on ${this.favSubjects[0]}`;
+  }
+}
+
+
+
+
+
 // Test
-const fred = new Person({
+const fred = new Instructor({
   name: 'Fred',
   location: 'Bedrock',
   age: 37,
-  // favLanguage: 'JavaScript',
-  // specialty: 'Front-end',
-  // catchPhrase: `Don't forget the homies`
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+const ignacio = new Student({
+  name: 'Ignacio',
+  location: 'Peru',
+  age: 39,
+  previousBackground: 'Finance',
+  className:'WEB24',
+  favSubjects: ['JavaScript','Python','Ruby']
 });
 
 console.log(fred.speak());
+console.log(fred.demo());
+console.log(fred.grade());
+console.log(ignacio.listsSubjects());
+console.log(ignacio.PRAssignment());
+console.log(ignacio.sprintChallenge());
